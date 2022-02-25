1dd14485052979f1513caee74c4757fbc21a872d
atenanaz <38736959+atenanaz@users.noreply.github.com>
2022-02-25 15:40:30 +0100

    Add files via upload
    created

---
+++ run_extractSpectrogram_faulty.m
@@ -0,0 +1,156 @@
+clear
+cnt = 1; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 2; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 3; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 4; 
+ExtractSpect_FaultyData
+
+clear
+cnt = 5; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 6; 
+ExtractSpect_FaultyData
+
+clear
+cnt = 7; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 8; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 9; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 10; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 11; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 12; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 13; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 14; 
+ExtractSpect_FaultyData 
+
+
+clear
+cnt = 15; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 16; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 17; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 18; 
+ExtractSpect_FaultyData
+
+clear
+cnt = 19; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 20; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 21; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 22; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 23; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 24; 
+ExtractSpect_FaultyData
+
+clear
+cnt = 25; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 26; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 27; 
+ExtractSpect_FaultyData
+
+clear
+cnt = 28; 
+ExtractSpect_FaultyData
+
+clear
+cnt = 29; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 30; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 31; 
+ExtractSpect_FaultyData
+
+clear
+cnt = 32; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 33; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 34; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 35; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 36; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 37; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 38; 
+ExtractSpect_FaultyData 
+
+clear
+cnt = 39; 
+ExtractSpect_FaultyData 
