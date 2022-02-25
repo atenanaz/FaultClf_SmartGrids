ba8ea210e21fae3422838873f14668e6f824a2dd
atenanaz <38736959+atenanaz@users.noreply.github.com>
2022-02-25 15:40:54 +0100

    Add files via upload
    created

---
+++ run_extractSpectrogram_healthy.m
@@ -0,0 +1,17 @@
+clear
+cnt = 1;
+extractSpectrogram_healthy
+
+
+clear
+cnt = 2;
+extractSpectrogram_healthy
+
+
+clear
+cnt = 3;
+extractSpectrogram_healthy
+
+clear
+cnt = 4;
+extractSpectrogram_healthy
